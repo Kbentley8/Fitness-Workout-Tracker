c3d447b65d19a732d400c47207b616e787ec8716
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-11-04 14:36:58 -0400

    updated seed file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/fittrack");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack");
 
 let workoutSeed = [
   {
